6d50dae1426a477b67cbc8519fb0c2d7660abf73
Mike Chan <mike.chan@kmdev.local>
2017-11-08 16:19:53 +0100

    Using HttpClient instead of Http.  Upgraded to Angular 4.3.0

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharepointContextService } from './sharepointcontext.service';
 import { KeywordSearchService } from './keywordsearch.service';
+import { HttpClientModule } from '@angular/common/http';
 
 export * from './sharepointitem.model';
 export * from './sharepointcontext.service';
@@ -10,7 +11,7 @@ export * from './httpBodyParser';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule, HttpClientModule
   ],
   declarations: [
     /*components,directives, pipes */
--- src/keywordsearch.service.ts
+++ src/keywordsearch.service.ts
@@ -1,10 +1,10 @@
-import { Http, Response } from '@angular/http';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import { Injectable } from '@angular/core';
 
 export interface ODataQueryConfiguration {
-    http: Http;
+    http: HttpClient;
     fieldsToSelect: string[];
     fieldsToSearch: string[];
     url: string;
